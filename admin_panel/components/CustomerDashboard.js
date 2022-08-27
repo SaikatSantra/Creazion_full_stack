e719bc414811b576f07d96af0c7486f0f0e052b4
SilverClouding <saikat1308003@gmail.com>
2022-08-27 14:24:54 +0530

    day 2

--- admin_panel/components/CustomerDashboard.js
+++ admin_panel/components/CustomerDashboard.js
@@ -15,38 +15,56 @@ export default function CustomerDashboard() {
     
   return (
     <div>
+
+<Box className='red' sx={{ flexGrow: 1 }}  style={{ marginBottom: '30px' }} >
+    <Grid container spacing={2}>
+    <CustomerOffer/>
+    </Grid>
+    </Box>
+
+
          <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-        <Grid xs={4}>
+
+        <Grid  xs={12} md={4} sm={4}>
           <Item>
             Hello,User Good Morning
           </Item>
         </Grid>
-        <Grid xs={4}>
+
+        <Grid  xs={12} md={4} sm={4}>
           <Item>Total Invesment:828828282</Item>
         </Grid>
         
-        <Grid xs={4}>
+        <Grid  xs={12} md={4} sm={4}>
           <Item>Total Profit:22727272</Item>
         </Grid>
+
       </Grid>
     </Box>
-        <CustomerOffer/>
+
+
+ 
+        
+     
+
+
+
         <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-        <Grid xs={4}>
+        <Grid  xs={12} md={4} sm={4}>
           <Item>
             Want to BA Model
           </Item>
         </Grid>
-        <Grid xs={4}>
+        <Grid  xs={12} md={4} sm={4}>
           <Item>Join CSP </Item>
           <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
           </p>
           <a href="#">Know More</a>
         </Grid>
         
-        <Grid xs={4}>
+        <Grid  xs={12} md={4} sm={4}>
           <Item>BRANCH Open</Item>
         </Grid>
       </Grid>
--- admin_panel/components/CustomerOffer.js
+++ admin_panel/components/CustomerOffer.js
@@ -1,9 +1,70 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 import Image from 'next/image'
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 export default function CustomerOffer() {
   return (
-    <div className='ms-5'>
-        <Image src="https://thumbs.dreamstime.com/z/special-offer-written-blackboard-special-offer-102131020.jpg" width={1000} height={200} style={{width:'100%'}}/>
-    </div>
+    <>
+     
+     <Swiper 
+      // install Swiper modules
+      modules={[Navigation, Pagination,  A11y]}
+      spaceBetween={0}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: false }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide  style={{ width:'100%'  }}>
+      <Grid item>
+      <Item>
+     <Image alt='' src="https://images.pexels.com/photos/5872364/pexels-photo-5872364.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={1980} height={350} />
+      </Item>
+      </Grid>
+      </SwiperSlide>
+      <SwiperSlide style={{ width:'100%' }}>
+      <Grid item>
+      <Item>
+     <Image alt='' src="https://thumbs.dreamstime.com/z/special-offer-written-blackboard-special-offer-102131020.jpg" width={1980} height={350}/>
+      </Item>
+      </Grid>
+      </SwiperSlide>
+    </Swiper>
+
+     {/* <Grid item xs={12} md={4} sm={6}>
+      <Item>
+     <Image src="https://thumbs.dreamstime.com/z/special-offer-written-blackboard-special-offer-102131020.jpg" width={1000} height={200} style={{width:'100%'}}/>
+      </Item>
+      </Grid>
+      <Grid item xs={12} md={4} sm={6}><Item>
+     <Image src="https://thumbs.dreamstime.com/z/special-offer-written-blackboard-special-offer-102131020.jpg" width={1000} height={200} style={{width:'100%'}}/>
+      </Item>
+      </Grid>
+      <Grid item xs={12} md={4} sm={6}><Item>
+     <Image src="https://thumbs.dreamstime.com/z/special-offer-written-blackboard-special-offer-102131020.jpg" width={1000} height={200} style={{width:'100%'}}/>
+      </Item>
+      </Grid> */}
+    </>
   )
 }
